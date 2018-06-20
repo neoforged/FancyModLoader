a7eda1a458bb74613679ed6971bc28558f5dcf1f
cpw <cpw@weeksfamily.ca>
2018-06-19 21:33:46 -0400

    Rename and move to somewhere a bit better.

--- src/main/java/net/minecraftforge/api/distmarker/OnlyIn.java
+++ src/main/java/net/minecraftforge/api/distmarker/OnlyIn.java
@@ -17,25 +17,34 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-package net.minecraftforge.api;
+package net.minecraftforge.api.distmarker;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 
 /**
- * Marks the associated element as being only available on a certain {@link Side}. This is
- * generally meant for internal Forge and FML use only and should only be used on mod classes
+ * Marks the associated element as being only available on a certain {@link Dist}.
+ *
+ * Classes, fields, methods and constructors can be marked as only available in a specific distribution
+ * based on the presence of this annotation.
+ *
+ *
+ * <p>This is generally meant for internal Forge and FML use only and should only be used on mod classes
  * when other more common mechanisms fail to work.
  *
+ *
  * Note, this will <em>only</em> apply to the direct element marked. This code:
- * <code> @SideOnly public MyField field = new MyField();</code> will <strong>not</strong> work, as the initializer
- * is a separate piece of code to the actual field declaration, and will not be able to find
+ * <code> @OnlyIn(Dist.CLIENT) public MyField field = new MyField();</code> will <strong>not</strong> work,
+ * as the initializer is a separate piece of code to the actual field declaration, and will not be able to find
  * it's field on the wrong side.
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.CONSTRUCTOR})
-public @interface SideOnly
+public @interface OnlyIn
 {
-    public Side value();
+    public Dist value();
 }
