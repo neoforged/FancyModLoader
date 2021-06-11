0428692ea4a5cdb843b220c2ea64a001c66b726e
cpw <cpw+github@weeksfamily.ca>
2021-06-11 00:50:24 -0400

    don't allow supplying the parent classloader, rather use the same way the Loader does it..
    Signed-off-by: cpw <cpw+github@weeksfamily.ca>

--- src/main/java/cpw/mods/cl/ModuleClassLoader.java
+++ src/main/java/cpw/mods/cl/ModuleClassLoader.java
@@ -9,6 +9,7 @@ import java.net.URI;
 import java.net.URL;
 import java.util.*;
 import java.util.function.BiFunction;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class ModuleClassLoader extends ClassLoader {
@@ -21,12 +22,10 @@ public class ModuleClassLoader extends ClassLoader {
     private final Configuration configuration;
     private final Map<String, JarModuleFinder.JarModuleReference> resolvedRoots;
     private final Map<String, ResolvedModule> packageLookup;
+    private final Map<String, ClassLoader> parentLoaders;
 
-    public ModuleClassLoader(final String name, final Configuration configuration) {
-        this(name, configuration, null);
-    }
-    public ModuleClassLoader(final String name, final Configuration configuration, ClassLoader parent) {
-        super(name, parent);
+    public ModuleClassLoader(final String name, final Configuration configuration, final List<ModuleLayer> parentLayers) {
+        super(name, null);
         this.configuration = configuration;
         record JarRef(ResolvedModule m, JarModuleFinder.JarModuleReference ref) {}
         this.resolvedRoots = configuration.modules().stream()
@@ -37,6 +36,30 @@ public class ModuleClassLoader extends ClassLoader {
         this.packageLookup = new HashMap<>();
         for (var mod : this.configuration.modules()) {
             mod.reference().descriptor().packages().forEach(pk->this.packageLookup.put(pk, mod));
+        }
+
+        this.parentLoaders = new HashMap<>();
+        for (var rm : configuration.modules()) {
+            for (var other : rm.reads()) {
+                Supplier<ClassLoader> findClassLoader = ()->{
+                    if (other.configuration() != configuration) {
+                        var pl = parentLayers.stream().filter(l -> l.configuration() == other.configuration()).findFirst().orElseThrow();
+                        return Optional.ofNullable(pl.findLoader(other.name())).orElse(ClassLoader.getPlatformClassLoader());
+                    } else {
+                        return ModuleClassLoader.this;
+                    }
+                };
+                var cl = findClassLoader.get();
+                final var descriptor = other.reference().descriptor();
+                if (descriptor.isAutomatic()) {
+                    descriptor.packages().forEach(pn->this.parentLoaders.put(pn, cl));
+                } else {
+                    descriptor.exports().stream()
+                            .filter(e -> !e.isQualified() || (e.isQualified() && other.configuration() == configuration && e.targets().contains(rm.name())))
+                            .map(ModuleDescriptor.Exports::source)
+                            .forEach(pn->this.parentLoaders.put(pn, cl));
+                }
+            }
         }
     }
 
@@ -72,6 +95,24 @@ public class ModuleClassLoader extends ClassLoader {
         var pkg = ProtectionDomainHelper.tryDefinePackage(this, name, modroot.jar().getManifest(), t->modroot.jar().getTrustedManifestEntries(t), this::definePackage);
         var cs = ProtectionDomainHelper.createCodeSource(toURL(ref.location()), modroot.jar().verifyAndGetSigners(cname, bytes));
         return defineClass(name, bytes, 0, bytes.length, ProtectionDomainHelper.createProtectionDomain(cs, this));
+    }
+
+    @Override
+    protected Class<?> loadClass(final String name, final boolean resolve) throws ClassNotFoundException {
+        synchronized (getClassLoadingLock(name)) {
+            var c = findLoadedClass(name);
+            if (c == null) {
+                final var pname = name.substring(0, name.lastIndexOf('.'));
+                if (this.packageLookup.containsKey(pname)) {
+                    c = findClass(this.packageLookup.get(pname).name(), name);
+                } else {
+                    c = this.parentLoaders.getOrDefault(pname, ClassLoader.getPlatformClassLoader()).loadClass(name);
+                }
+            }
+            if (c == null) throw new ClassNotFoundException(name);
+            if (resolve) resolveClass(c);
+            return c;
+        }
     }
 
     @Override
