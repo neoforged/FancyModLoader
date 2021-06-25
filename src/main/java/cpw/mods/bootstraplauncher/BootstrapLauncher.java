6eae69063ded8973350f437f5449c8f7d12fdbfc
cpw <cpw+github@weeksfamily.ca>
2021-06-25 00:42:53 -0400

    add in all the modules
    Signed-off-by: cpw <cpw+github@weeksfamily.ca>

--- src/main/java/cpw/mods/bootstraplauncher/BootstrapLauncher.java
+++ src/main/java/cpw/mods/bootstraplauncher/BootstrapLauncher.java
@@ -3,37 +3,40 @@ package cpw.mods.bootstraplauncher;
 import cpw.mods.cl.JarModuleFinder;
 import cpw.mods.cl.ModuleClassLoader;
 import cpw.mods.jarhandling.SecureJar;
-import cpw.mods.jarhandling.impl.Jar;
 
 import java.io.File;
 import java.lang.module.ModuleFinder;
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.ServiceLoader;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class BootstrapLauncher {
     @SuppressWarnings("unchecked")
-    public static void main(String[] args) throws URISyntaxException {
+    public static void main(String[] args) {
         var legacyCP = Objects.requireNonNull(System.getProperty("legacyClassPath"), "Missing legacyClassPath, cannot bootstrap");
         var versionName = Objects.requireNonNull(System.getProperty("versionName"), "Missing versionName, cannot bootstrap");
 
         var fileList = Arrays.stream(legacyCP.split(File.pathSeparator))
                 .filter(n->!n.endsWith(versionName+".jar"))
+                .filter(n->!n.contains("/org/ow2/asm/"))
                 .map(s->URI.create("file://"+s))
                 .collect(Collectors.toList());
         Collections.reverse(fileList);
-        var jf = JarModuleFinder.of(fileList.stream()
+        var finder = fileList.stream()
                 .map(Path::of)
                 .map(SecureJar::from)
-                .toArray(SecureJar[]::new));
+                .toArray(SecureJar[]::new);
+        var alltargets = Arrays.stream(finder).map(SecureJar::name).toList();
+        var jf = JarModuleFinder.of(finder);
         var cf = ModuleLayer.boot().configuration();
-        var newcf = cf.resolveAndBind(jf, ModuleFinder.ofSystem(), List.of("cpw.mods.bootstraplauncher"));
+        var newcf = cf.resolveAndBind(jf, ModuleFinder.ofSystem(), alltargets);
         var mycl = new ModuleClassLoader("MC-BOOTSTRAP", newcf, List.of(ModuleLayer.boot()));
         var layer = ModuleLayer.defineModules(newcf, List.of(ModuleLayer.boot()), m->mycl);
         Thread.currentThread().setContextClassLoader(mycl);
