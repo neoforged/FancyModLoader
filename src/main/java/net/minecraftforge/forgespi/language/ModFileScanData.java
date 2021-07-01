71a605a07614169117f47fd202ff6ba4fa09b8e2
cpw <cpw+github@weeksfamily.ca>
2021-07-01 00:06:59 -0400

    allow multiple language loaders in scan data, and use records :D
    Signed-off-by: cpw <cpw+github@weeksfamily.ca>

--- src/main/java/net/minecraftforge/forgespi/language/ModFileScanData.java
+++ src/main/java/net/minecraftforge/forgespi/language/ModFileScanData.java
@@ -21,12 +21,7 @@ package net.minecraftforge.forgespi.language;
 
 
 import java.lang.annotation.ElementType;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Predicate;
 
 import org.objectweb.asm.Type;
@@ -35,7 +30,7 @@ public class ModFileScanData
 {
     private final Set<AnnotationData> annotations = new LinkedHashSet<>();
     private final Set<ClassData> classes = new LinkedHashSet<>();
-    private Map<String,? extends IModLanguageProvider.IModLanguageLoader> modTargets;
+    private final Map<String,IModLanguageProvider.IModLanguageLoader> modTargets = new HashMap<>();
     private Map<String,?> functionalScanners;
     private List<IModFileInfo> modFiles = new ArrayList<>();
 
@@ -51,9 +46,9 @@ public class ModFileScanData
         return annotations;
     }
 
-    public void addLanguageLoader(Map<String,? extends IModLanguageProvider.IModLanguageLoader> modTargetMap)
+    public void addLanguageLoader(final Map<String,? extends IModLanguageProvider.IModLanguageLoader> modTargetMap)
     {
-        modTargets = modTargetMap;
+        modTargets.putAll(modTargetMap);
     }
 
     public void addModFileInfo(IModFileInfo info) {
@@ -69,84 +64,7 @@ public class ModFileScanData
         return this.modFiles;
     }
 
-    public static class ClassData {
-        private final Type clazz;
-        private final Type parent;
-        private final Set<Type> interfaces;
+    public record ClassData(Type clazz, Type parent, Set<Type> interfaces) {}
 
-        public ClassData(final Type clazz, final Type parent, final Set<Type> interfaces) {
-            this.clazz = clazz;
-            this.parent = parent;
-            this.interfaces = interfaces;
-        }
-
-        @Override
-        public boolean equals(final Object obj) {
-            try {
-                return (!Objects.isNull(obj)) && Objects.equals(clazz, ((ClassData)obj).clazz);
-            } catch (ClassCastException e) {
-                return false;
-            }
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hashCode(this.clazz);
-        }
-    }
-    public static class AnnotationData {
-        private final Type annotationType;
-        private final ElementType targetType;
-        private final Type clazz;
-        private final String memberName;
-        private final Map<String,Object> annotationData;
-
-        public AnnotationData(final Type annotationType, final ElementType targetType, final Type clazz, final String memberName, final Map<String, Object> annotationData) {
-            this.annotationType = annotationType;
-            this.targetType = targetType;
-            this.clazz = clazz;
-            this.memberName = memberName;
-            this.annotationData = annotationData;
-        }
-
-        public Type getAnnotationType() {
-            return annotationType;
-        }
-
-        public ElementType getTargetType() {
-            return targetType;
-        }
-
-        public Type getClassType() {
-            return clazz;
-        }
-
-        public String getMemberName() {
-            return memberName;
-        }
-
-        public Map<String, Object> getAnnotationData() {
-            return annotationData;
-        }
-
-        @Override
-        public boolean equals(final Object obj) {
-            if (obj == null) { return false; }
-            if (obj == this) { return true; }
-            if (obj.getClass() != getClass()) {
-                return false;
-            }
-            AnnotationData dat = (AnnotationData) obj;
-            return Objects.equals(annotationType, dat.annotationType)
-        		&& Objects.equals(targetType, dat.targetType)
-        		&& Objects.equals(clazz, dat.clazz)
-        		&& Objects.equals(memberName, dat.memberName)
-        		&& Objects.equals(annotationData, dat.annotationData);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(annotationType, targetType, clazz, memberName, annotationData);
-        }
-    }
+    public record AnnotationData(Type annotationType, ElementType targetType, Type clazz, String memberName, Map<String, Object> annotationData) {}
 }
