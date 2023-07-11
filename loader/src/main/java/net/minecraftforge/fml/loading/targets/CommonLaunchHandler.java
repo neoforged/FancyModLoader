c6744d43a62cf22caab70f7cc5baceb6b9ef7771
cpw <cpw+github@weeksfamily.ca>
2023-07-11 17:41:12 -0400

    We're fml_loader now

--- loader/src/main/java/net/minecraftforge/fml/loading/targets/CommonLaunchHandler.java
+++ loader/src/main/java/net/minecraftforge/fml/loading/targets/CommonLaunchHandler.java
@@ -57,7 +57,7 @@ public abstract class CommonLaunchHandler implements ILaunchHandlerService {
 
     protected String[] preLaunch(String[] arguments, ModuleLayer layer) {
         URI uri;
-        try (var reader = layer.configuration().findModule("loader").orElseThrow().reference().open()) {
+        try (var reader = layer.configuration().findModule("fml_loader").orElseThrow().reference().open()) {
             uri = reader.find("log4j2.xml").orElseThrow();
         } catch (IOException e) {
             throw new RuntimeException(e);
