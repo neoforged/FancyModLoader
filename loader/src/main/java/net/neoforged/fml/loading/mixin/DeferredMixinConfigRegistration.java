f989ea07e93542210c1faa02f33824f0415ffaeb
Technici4n <13494793+Technici4n@users.noreply.github.com>
2024-01-29 20:33:42 +0100

    Provide mixin config sources

--- loader/src/main/java/net/neoforged/fml/loading/mixin/DeferredMixinConfigRegistration.java
+++ loader/src/main/java/net/neoforged/fml/loading/mixin/DeferredMixinConfigRegistration.java
@@ -5,16 +5,18 @@
 
 package net.neoforged.fml.loading.mixin;
 
+import net.neoforged.neoforgespi.locating.IModFile;
 import org.spongepowered.asm.launch.GlobalProperties;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigSource;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DeferredMixinConfigRegistration {
     private static boolean added = false;
-    private static final List<String> mixinConfigs = new ArrayList<>();
+    private static final List<MixinToAdd> mixinConfigs = new ArrayList<>();
 
     static {
         // Register our platform agent first
@@ -24,17 +26,38 @@ public class DeferredMixinConfigRegistration {
         MixinBootstrap.getPlatform().addContainer(new FMLMixinContainerHandle());
     }
 
-    public static void addMixinConfig(String config) {
+    private record MixinToAdd(String config, IMixinConfigSource source) {}
+
+    public static void addCommandLineMixinConfig(String config) {
+        addMixinConfig(config, new MixinConfigSource(config, "Mixin config passed via --fml.mixinConfig " + config));
+    }
+
+    public static void addModMixinConfig(String config, IModFile file) {
+        String sourceId;
+        if (file.getModInfos().size() == 1) {
+            // Single mod, use the modid
+            sourceId = file.getModInfos().get(0).getModId();
+        } else {
+            // Else use file name...
+            sourceId = file.getFileName();
+        }
+
+        addMixinConfig(config, new MixinConfigSource(sourceId, "Mixin config loaded from mod file " + file.getFilePath()));
+    }
+
+    private static void addMixinConfig(String config, IMixinConfigSource source) {
         if (added) {
             throw new IllegalStateException("Too late to add mixin configs!");
         }
 
-        mixinConfigs.add(config);
+        mixinConfigs.add(new MixinToAdd(config, source));
     }
 
     static void registerConfigs() {
         added = true;
-        mixinConfigs.forEach(Mixins::addConfiguration);
+        mixinConfigs.forEach(c -> {
+            Mixins.addConfiguration(c.config(), c.source());
+        });
         mixinConfigs.clear();
     }
 }
