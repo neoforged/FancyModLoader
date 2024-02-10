b6b267ebf10cb3569ffcbb16052df01f95893107
Matyrobbrt <matyrobbrt@gmail.com>
2024-02-11 00:09:32 +0200

    Add headers to crash report extenders

--- loader/src/main/java/net/neoforged/fml/ISystemReportExtender.java
+++ loader/src/main/java/net/neoforged/fml/ISystemReportExtender.java
@@ -5,14 +5,39 @@
 
 package net.neoforged.fml;
 
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.function.Supplier;
 
-public interface ISystemReportExtender extends Supplier<String>
-{
-    String getLabel();
+@ApiStatus.OverrideOnly
+public interface ISystemReportExtender extends Supplier<String> {
+    /**
+     * {@return the label of the crash report value to add}
+     */
+    @Nullable
+    default String getLabel() {
+        return null;
+    }
 
-    default boolean isActive()
-    {
+    /**
+     * {@return the value to add to the crash report}
+     */
+    @Nullable
+    @Override
+    default String get() {
+        return null;
+    }
+
+    /**
+     * {@return a header to add to the crash report}
+     */
+    @Nullable
+    default String getHeader() {
+        return null;
+    }
+
+    default boolean isActive() {
         return true;
     }
 }
