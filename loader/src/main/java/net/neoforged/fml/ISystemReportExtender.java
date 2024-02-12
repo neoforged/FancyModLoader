67f51640928586b4dde04441bc97f3aad263978c
Matyrobbrt <65940752+Matyrobbrt@users.noreply.github.com>
2024-02-12 18:16:57 +0200

    Add extensible crash report header system (#88)

--- loader/src/main/java/net/neoforged/fml/ISystemReportExtender.java
+++ loader/src/main/java/net/neoforged/fml/ISystemReportExtender.java
@@ -5,14 +5,16 @@
 
 package net.neoforged.fml;
 
+import org.jetbrains.annotations.ApiStatus;
+
 import java.util.function.Supplier;
 
+@ApiStatus.OverrideOnly
 public interface ISystemReportExtender extends Supplier<String>
 {
     String getLabel();
 
-    default boolean isActive()
-    {
+    default boolean isActive() {
         return true;
     }
 }
