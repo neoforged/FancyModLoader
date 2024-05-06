fde6bfa3740172b75ff00a53e5a0815af7ab4f91
Matyrobbrt <65940752+Matyrobbrt@users.noreply.github.com>
2024-05-06 23:58:32 +0300

    Allow FML translations to use the vanilla format specifier (#134)

---
+++ loader/src/test/java/net/neoforged/fml/test/TranslationPatternsTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+package net.neoforged.fml.test;
+
+import net.neoforged.fml.i18n.FMLTranslations;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TranslationPatternsTest {
+    @Test
+    void testVanillaFormatSpecifiers() {
+        Assertions.assertThat(FMLTranslations.parseFormat("this is a translation %s %s", "a", "b"))
+                .isEqualTo("this is a translation a b");
+
+        Assertions.assertThat(FMLTranslations.parseFormat("this is a translation %2$s %1$s", "a", "b"))
+                .isEqualTo("this is a translation b a");
+    }
+
+    @Test
+    void testDoublePercent() {
+        Assertions.assertThat(FMLTranslations.parseFormat("%%")).isEqualTo("%");
+    }
+}
