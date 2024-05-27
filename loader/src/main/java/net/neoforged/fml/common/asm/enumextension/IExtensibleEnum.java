d18f97833d35f76cbf2eba651c4812844805d202
XFactHD <xfacthd@gmx.de>
2024-05-27 23:59:22 +0200

    Use code snippet in IExtensibleEnum doc

--- loader/src/main/java/net/neoforged/fml/common/asm/enumextension/IExtensibleEnum.java
+++ loader/src/main/java/net/neoforged/fml/common/asm/enumextension/IExtensibleEnum.java
@@ -10,13 +10,12 @@ package net.neoforged.fml.common.asm.enumextension;
  * extensible. If this is implemented on a class, the class must define a static
  * method called {@code isExtended()} which takes zero args and returns a boolean.
  * By default, the method should throw to make sure the enum was handled by the transformer.
- * <p>
  *
- * <pre>
+ * {@snippet :
  * public static boolean isExtended() {
  *     throw new IllegalStateException("Enum not transformed");
  * }
- * </pre>
+ * }
  *
  * The method contents will be replaced with ASM at runtime to return whether any
  * modded were added to the enum
