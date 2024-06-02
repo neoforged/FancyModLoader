02c2e73818ad66a22dc348a98201c0259e749451
Sebastian Hartte <shartte@users.noreply.github.com>
2024-06-02 11:51:30 +0200

    Formatting

--- loader/src/main/java/net/neoforged/fml/loading/FMLServiceProvider.java
+++ loader/src/main/java/net/neoforged/fml/loading/FMLServiceProvider.java
@@ -16,7 +16,6 @@ import cpw.mods.modlauncher.api.ITransformer;
 import cpw.mods.modlauncher.api.IncompatibleEnvironmentException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
