c826dc0d90478e5f556a42553a80816c3b2e4eb4
Technici4n <13494793+Technici4n@users.noreply.github.com>
2024-07-05 23:54:10 +0200

    Licenses

--- loader/src/main/java/net/neoforged/fml/config/ConfigWatcher.java
+++ loader/src/main/java/net/neoforged/fml/config/ConfigWatcher.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.neoforged.fml.config;
 
 import com.electronwill.nightconfig.core.file.CommentedFileConfig;
