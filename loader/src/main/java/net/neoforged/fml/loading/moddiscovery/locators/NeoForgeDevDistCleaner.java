318697c7e38998ddd56e59910765bbd92a4a8e64
Luke Bemish <lukebemish@lukebemish.dev>
2025-06-17 17:39:49 -0500

    Fix license headers

--- loader/src/main/java/net/neoforged/fml/loading/moddiscovery/locators/NeoForgeDevDistCleaner.java
+++ loader/src/main/java/net/neoforged/fml/loading/moddiscovery/locators/NeoForgeDevDistCleaner.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.neoforged.fml.loading.moddiscovery.locators;
 
 import com.mojang.logging.LogUtils;
