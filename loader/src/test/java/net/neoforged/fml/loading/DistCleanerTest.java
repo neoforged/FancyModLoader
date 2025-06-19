858aa2663b72fd2186d3e4bd234e1825a727d65a
Luke Bemish <lukebemish@lukebemish.dev>
2025-06-18 21:03:45 -0500

    Fix up test to check for resource filtering

--- loader/src/test/java/net/neoforged/fml/loading/DistCleanerTest.java
+++ loader/src/test/java/net/neoforged/fml/loading/DistCleanerTest.java
@@ -13,12 +13,13 @@ import com.google.common.jimfs.Jimfs;
 import java.io.ByteArrayOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.List;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 import net.neoforged.fml.test.RuntimeCompiler;
 import org.junit.jupiter.api.Test;
 
-class RuntimeDistCleanerTest extends LauncherTest {
+class DistCleanerTest extends LauncherTest {
     @Test
     void testUserDevDistCleaning() throws Exception {
         var classpath = installation.setupUserdevProject();
@@ -35,7 +36,7 @@ class RuntimeDistCleanerTest extends LauncherTest {
         manifest.getAttributes(maskedClassPath).putValue("Minecraft-Dist", "client");
         var bout = new ByteArrayOutputStream();
         manifest.write(bout);
-        var content = bout.toByteArray();
+        var manifestBytes = bout.toByteArray();
 
         var memoryFs = Jimfs.newFileSystem(Configuration.unix());
         var compiler = new RuntimeCompiler(memoryFs);
@@ -50,18 +51,36 @@ class RuntimeDistCleanerTest extends LauncherTest {
                 }""");
         compilationBuilder.compile();
 
+        var manifestContent = new IdentifiableContent("MANIFEST", "META-INF/MANIFEST.MF", manifestBytes);
+        var maskedResourceContent = new IdentifiableContent("MASKED_RESOURCE", maskedResourcePath, "{}".getBytes(StandardCharsets.UTF_8));
+        var maskedClassContent = new IdentifiableContent("MASKED_CLASS", maskedClassPath,
+                Files.readAllBytes(memoryFs.getPath("/", maskedClassPath)));
+        var loadsMaskedClassContent = new IdentifiableContent("LOADS_MASKED_CLASS", loadsMaskedClassPath,
+                Files.readAllBytes(memoryFs.getPath("/", loadsMaskedClassPath)));
+        var clientAssetsContent = new IdentifiableContent("CLIENT_ASSETS", "assets/.mcassetsroot");
+        var sharedAssetsContent = new IdentifiableContent("SHARED_ASSETS", "data/.mcassetsroot");
+        
         SimulatedInstallation.writeJarFile(clientExtraJar,
-                new IdentifiableContent("MANIFEST", "META-INF/MANIFEST.MF", content),
-                new IdentifiableContent("MASKED_RESOURCE", maskedResourcePath, "{}".getBytes(StandardCharsets.UTF_8)),
-                new IdentifiableContent("MASKED_CLASS", maskedClassPath,
-                        Files.readAllBytes(memoryFs.getPath("/", maskedClassPath))),
-                new IdentifiableContent("LOADS_MASKED_CLASS", loadsMaskedClassPath,
-                        Files.readAllBytes(memoryFs.getPath("/", loadsMaskedClassPath))),
-                new IdentifiableContent("CLIENT_ASSETS", "assets/.mcassetsroot"),
-                new IdentifiableContent("SHARED_ASSETS", "data/.mcassetsroot"));
+                manifestContent,
+                maskedResourceContent,
+                maskedClassContent,
+                loadsMaskedClassContent,
+                clientAssetsContent,
+                sharedAssetsContent);
 
         var result = launchAndLoadWithAdditionalClasspath("neoforgeserverdev", classpath);
         assertThat(result.issues()).isEmpty();
+        installation.assertModContent(result, "minecraft", List.of(
+                manifestContent,
+                // Masked classes are still present, as these are removed on class load for the more specific error message
+                maskedClassContent,
+                loadsMaskedClassContent,
+                clientAssetsContent,
+                sharedAssetsContent,
+                // Other resources from the main jar
+                SimulatedInstallation.generateClass("PATCHED_CLIENT", "net/minecraft/client/Minecraft.class"),
+                SimulatedInstallation.generateClass("PATCHED_SHARED", "net/minecraft/server/MinecraftServer.class")
+        ));
         assertThatThrownBy(() -> Class.forName("test.Masked", true, gameClassLoader))
                 .isExactlyInstanceOf(ClassNotFoundException.class).hasMessage("Attempted to load class test.Masked which is not present on the dedicated server");
         assertThatThrownBy(() -> Class.forName("test.LoadsMasked", true, gameClassLoader))
