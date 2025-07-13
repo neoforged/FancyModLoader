c744ae7959e638a04a4cf9fd6e8a62f7c4b9e4db
Sebastian Hartte <shartte@users.noreply.github.com>
2025-07-13 22:18:25 +0200

    Add tests that check when and which ATs are applied.

--- loader/src/test/java/net/neoforged/fml/loading/AccessTransformerTest.java
+++ loader/src/test/java/net/neoforged/fml/loading/AccessTransformerTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.neoforged.fml.loading;
 
 import static org.assertj.core.api.Assertions.assertThat;
