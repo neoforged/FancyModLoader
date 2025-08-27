4004132486771af902cbfb2bb59710f6a414ac31
Sebastian Hartte <shartte@users.noreply.github.com>
2025-08-28 01:14:08 +0200

    Trying to debug test failure.

--- testlib/src/main/java/net/neoforged/fml/testlib/ModFileBuilder.java
+++ testlib/src/main/java/net/neoforged/fml/testlib/ModFileBuilder.java
@@ -16,7 +16,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
