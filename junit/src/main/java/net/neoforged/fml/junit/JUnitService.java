030dcd9744a1d1f3debc9cffb10b0cd145169739
Sebastian Hartte <shartte@users.noreply.github.com>
2025-09-07 17:51:13 +0200

    JUnit fixes

--- junit/src/main/java/net/neoforged/fml/junit/JUnitService.java
+++ junit/src/main/java/net/neoforged/fml/junit/JUnitService.java
@@ -5,29 +5,91 @@
 
 package net.neoforged.fml.junit;
 
+import java.nio.file.Path;
+import java.util.List;
+import java.util.ServiceLoader;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModLoadingException;
+import net.neoforged.fml.loading.FMLLoader;
+import net.neoforged.fml.loading.LoadingModList;
+import net.neoforged.fml.startup.JUnitGameBootstrapper;
+import net.neoforged.fml.startup.StartupArgs;
+import org.junit.platform.engine.support.store.Namespace;
 import org.junit.platform.launcher.LauncherSession;
 import org.junit.platform.launcher.LauncherSessionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A session listener for JUnit environments that will bootstrap a Minecraft (FML) environment.
  */
 public class JUnitService implements LauncherSessionListener {
-    private ClassLoader oldLoader;
+    private static final Logger LOG = LoggerFactory.getLogger(JUnitService.class);
+
+    private static final Namespace NAMESPACE = Namespace.create("fml", "junit");
+    static final String KEY_ACTIVE_LOADER = "activeLoader";
 
     public JUnitService() {}
 
+    record ActivatedLoader(
+            ClassLoader previousLoader,
+            FMLLoader loader) implements AutoCloseable {
+        @Override
+        public void close() {
+            loader.close();
+            Thread.currentThread().setContextClassLoader(previousLoader);
+        }
+    }
+
     @Override
     public void launcherSessionOpened(LauncherSession session) {
+        var store = session.getStore();
+
         // When the tests are started we want to make sure that they run on the transforming class loader which is set up by
         // bootstrapping BSL which will then load the launch target
-        oldLoader = Thread.currentThread().getContextClassLoader();
-        Thread.currentThread().setContextClassLoader(LaunchWrapper.getTransformingLoader());
+        var loader = store.getOrComputeIfAbsent(NAMESPACE, KEY_ACTIVE_LOADER, ignored -> createLoader(), ActivatedLoader.class);
+        LOG.info("Active FML loader {}", Integer.toHexString(System.identityHashCode(loader.loader)));
     }
 
     @Override
     public void launcherSessionClosed(LauncherSession session) {
-        // Reset the loader in case JUnit wants to execute some pre-shutdown commands
-        // and our custom class loader might throw it off
-        Thread.currentThread().setContextClassLoader(oldLoader);
+        var loader = session.getStore().remove(NAMESPACE, KEY_ACTIVE_LOADER, ActivatedLoader.class);
+        if (loader != null) {
+            loader.close();
+        }
+    }
+
+    private ActivatedLoader createLoader() {
+        var previousLoader = Thread.currentThread().getContextClassLoader();
+
+        long fmlStart = System.nanoTime();
+
+        // Start up FML
+        var startupArgs = new StartupArgs(
+                Path.of(""),
+                true,
+                Dist.DEDICATED_SERVER,
+                false,
+                new String[] {},
+                Set.of(),
+                List.of(),
+                previousLoader);
+        var loader = FMLLoader.create(null, startupArgs);
+
+        LOG.info("Starting FML took {}ms", TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - fmlStart));
+
+        if (LoadingModList.get().hasErrors()) {
+            throw new ModLoadingException(LoadingModList.get().getModLoadingIssues());
+        }
+
+        for (var bootstrapper : ServiceLoader.load(JUnitGameBootstrapper.class, loader.currentClassLoader())) {
+            long bootstrapStart = System.nanoTime();
+            bootstrapper.bootstrap(loader);
+            LOG.info("Running game bootstrapper {} took {}ms", bootstrapper.getClass().getName(), TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - bootstrapStart));
+        }
+
+        return new ActivatedLoader(previousLoader, loader);
     }
 }
