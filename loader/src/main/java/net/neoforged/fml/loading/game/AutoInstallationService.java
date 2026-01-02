fcc6ce860a5d43b71265801ff78febd4b5d0a5ad
Marc Hermans <marc.hermans@ldtteam.com>
2026-01-02 19:59:43 +0100

    Fix license violations

--- loader/src/main/java/net/neoforged/fml/loading/game/AutoInstallationService.java
+++ loader/src/main/java/net/neoforged/fml/loading/game/AutoInstallationService.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 package net.neoforged.fml.loading.game;
 
 import static net.neoforged.fml.loading.game.GameDiscovery.LIBRARIES_DIRECTORY_PROPERTY;
