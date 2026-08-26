95be1ff50cbf9be34b5d0fc888dc3362b6773607
XFactHD <11262040+XFactHD@users.noreply.github.com>
2026-08-26 14:28:45 +0000

    Backport #427 to 1.21.1

--- loader/src/main/java/net/neoforged/fml/loading/ModSorter.java
+++ loader/src/main/java/net/neoforged/fml/loading/ModSorter.java
@@ -32,7 +32,6 @@ import java.util.stream.Stream;
 import net.neoforged.fml.ModLoadingException;
 import net.neoforged.fml.ModLoadingIssue;
 import net.neoforged.fml.loading.moddiscovery.ModFile;
-import net.neoforged.fml.loading.moddiscovery.ModFileInfo;
 import net.neoforged.fml.loading.moddiscovery.ModInfo;
 import net.neoforged.fml.loading.toposort.CyclePresentException;
 import net.neoforged.fml.loading.toposort.TopologicalSort;
@@ -144,14 +143,15 @@ public class ModSorter {
         try {
             sorted = TopologicalSort.topologicalSort(graph, Comparator.comparing(infos::get));
         } catch (CyclePresentException e) {
-            Set<Set<ModFileInfo>> cycles = e.getCycles();
+            Set<Set<ModInfo>> cycles = e.getCycles();
             if (LOGGER.isErrorEnabled(LogMarkers.LOADING)) {
                 LOGGER.error(LogMarkers.LOADING, "Mod Sorting failed.\nDetected Cycles: {}\n", cycles);
             }
             var dataList = cycles.stream()
-                    .<ModFileInfo>mapMulti(Iterable::forEach)
-                    .<IModInfo>mapMulti((mf, c) -> mf.getMods().forEach(c))
-                    .map(IModInfo::getModId)
+                    .map(cycle -> cycle.stream()
+                            .map(IModInfo::getModId)
+                            .sorted()
+                            .collect(Collectors.joining(", ")))
                     .map(list -> ModLoadingIssue.error("fml.modloadingissue.cycle", list).withCause(e))
                     .toList();
             throw new ModLoadingException(dataList);
