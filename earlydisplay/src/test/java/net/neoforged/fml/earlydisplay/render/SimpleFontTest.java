1b5fca62c08aefac6ccf8b40ffece7195735d975
XFactHD <11262040+XFactHD@users.noreply.github.com>
2026-09-13 20:09:53 +0200

    Add back license headers

--- earlydisplay/src/test/java/net/neoforged/fml/earlydisplay/render/SimpleFontTest.java
+++ earlydisplay/src/test/java/net/neoforged/fml/earlydisplay/render/SimpleFontTest.java
@@ -1,4 +1,9 @@
 /*
+ * Copyright (c) NeoForged and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+/*
 package net.neoforged.fml.earlydisplay.render;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
